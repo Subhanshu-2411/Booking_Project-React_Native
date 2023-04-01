7aa84728924963c4abce508ff03eae5ea8438b74
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-01 16:32:34 +0530

    Added Modal Package

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from "./StackNavigator";
+import {ModalPortal} from "react-native-modals";
 
 export default function App() {
-  return (
-    <StackNavigator/>
-  );
+    return (
+        <>
+            <StackNavigator/>
+            <ModalPortal/>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
